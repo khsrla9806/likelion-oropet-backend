19043831e884b81ba90bc70d5dce38b0548d352a
khsrla9806 <kimhunsope@gmail.com>
2022-08-12 00:04:46 +0900

    add settings.py + CORS_ALLOWED_ORIGIN

--- backend/settings.py
+++ backend/settings.py
@@ -97,6 +97,13 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
+
+# 모든 CORS ORIGIN 허가
+# CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
