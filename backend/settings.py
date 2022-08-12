900eda9e93f938c03a4c15c53348484b7101cd1c
khsrla9806 <kimhunsope@gmail.com>
2022-08-12 22:18:23 +0900

    csrf and cookie setting

--- backend/settings.py
+++ backend/settings.py
@@ -183,3 +183,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
+
+SESSION_COOKIE_SAMESITE = None
